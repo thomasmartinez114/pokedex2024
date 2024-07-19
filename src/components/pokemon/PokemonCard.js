d8ad0340762b5f3328cce41c1b20cbc1d300ad2c
Thomas Martinez <devtommie114@gmail.com>
2024-07-19 19:11:19 -0400

    image showing with spinner when loading

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Spinner from '../pokemon/spinner.gif';
 
 const Sprite = styled.img`
   width: 5em;
   height: 5em;
+  display: none;
 `;
 
 export default class PokemonCard extends Component {
@@ -33,12 +35,31 @@ export default class PokemonCard extends Component {
       <div className='col-md-3 col-sm-6 mb-5'>
         <div className='card'>
           <h5 className='card-header'>{this.state.pokemonIndex}</h5>
+          {this.state.imageLoading ? (
+            <img
+              src={Spinner}
+              style={{ width: '5em', height: '5em' }}
+              className='card-img-top rounded mx-auto d-block mt-2'
+            />
+          ) : null}
           <Sprite
-            className='card-img-top rounded mx-auto mt=2'
+            className='card-img-top rounded mx-auto mt-2'
             onLoad={() => this.setState({ imageLoading: false })}
             onError={() => this.setState({ tooManyRequests: true })}
             src={this.state.imageUrl}
-          ></Sprite>
+            style={
+              this.state.tooManyRequests
+                ? { display: 'none' }
+                : this.state.imageLoading
+                ? null
+                : { display: 'block' }
+            }
+          />
+          {this.state.tooManyRequests ? (
+            <h6 className='mx-auto'>
+              <span className='badge badge-danger mt-2'>Too Many Requests</span>
+            </h6>
+          ) : null}
           <div className='card-body mx-auto'>
             <h6 className='card-title '>
               {this.state.name
