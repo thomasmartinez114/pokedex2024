bfd757821c743da2f557172d739a0abbdfcd4c53
Thomas Martinez <devtommie114@gmail.com>
2024-07-19 17:27:27 -0400

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from './components/layout/NavBar';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <NavBar />
     </div>
   );
 }
