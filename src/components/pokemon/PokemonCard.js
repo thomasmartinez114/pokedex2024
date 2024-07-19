a5b171613a84c39e2ad2bdf31cfe1fdd3f06f46d
Thomas Martinez <devtommie114@gmail.com>
2024-07-19 19:22:19 -0400

    added animiationa nd shadow to card

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Spinner from '../pokemon/spinner.gif';
+import spinner from '../pokemon/spinner.gif';
 
 const Sprite = styled.img`
   width: 5em;
   height: 5em;
   display: none;
+`;
+
+const Card = styled.div`
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)
+  }
 `;
 
 export default class PokemonCard extends Component {
@@ -33,11 +41,11 @@ export default class PokemonCard extends Component {
   render() {
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
-        <div className='card'>
+        <Card className='card'>
           <h5 className='card-header'>{this.state.pokemonIndex}</h5>
           {this.state.imageLoading ? (
             <img
-              src={Spinner}
+              src={spinner}
               style={{ width: '5em', height: '5em' }}
               className='card-img-top rounded mx-auto d-block mt-2'
             />
@@ -71,7 +79,7 @@ export default class PokemonCard extends Component {
                 .join(' ')}
             </h6>
           </div>
-        </div>
+        </Card>
       </div>
     );
   }
