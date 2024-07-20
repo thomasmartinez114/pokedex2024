c6c99b9a0c147b6c40e8299d51d4137a825825b4
Thomas Martinez <devtommie114@gmail.com>
2024-07-19 21:09:25 -0400

    changed Link to StyledLink via styled-component

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import spinner from '../pokemon/spinner.gif';
 
@@ -18,6 +19,18 @@ const Card = styled.div`
   -website-user-select: none;
   user-select: none;
   -o-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
 `;
 
 export default class PokemonCard extends Component {
@@ -45,45 +58,50 @@ export default class PokemonCard extends Component {
   render() {
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
-        <Card className='card'>
-          <h5 className='card-header'>{this.state.pokemonIndex}</h5>
-          {this.state.imageLoading ? (
-            <img
-              src={spinner}
-              style={{ width: '5em', height: '5em' }}
-              className='card-img-top rounded mx-auto d-block mt-2'
+        <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+          <Card className='card'>
+            <h5 className='card-header'>{this.state.pokemonIndex}</h5>
+            {this.state.imageLoading ? (
+              <img
+                src={spinner}
+                style={{ width: '5em', height: '5em' }}
+                className='card-img-top rounded mx-auto d-block mt-2'
+              />
+            ) : null}
+            <Sprite
+              className='card-img-top rounded mx-auto mt-2'
+              onLoad={() => this.setState({ imageLoading: false })}
+              onError={() => this.setState({ tooManyRequests: true })}
+              src={this.state.imageUrl}
+              style={
+                this.state.tooManyRequests
+                  ? { display: 'none' }
+                  : this.state.imageLoading
+                  ? null
+                  : { display: 'block' }
+              }
             />
-          ) : null}
-          <Sprite
-            className='card-img-top rounded mx-auto mt-2'
-            onLoad={() => this.setState({ imageLoading: false })}
-            onError={() => this.setState({ tooManyRequests: true })}
-            src={this.state.imageUrl}
-            style={
-              this.state.tooManyRequests
-                ? { display: 'none' }
-                : this.state.imageLoading
-                ? null
-                : { display: 'block' }
-            }
-          />
-          {this.state.tooManyRequests ? (
-            <h6 className='mx-auto'>
-              <span className='badge badge-danger mt-2'>Too Many Requests</span>
-            </h6>
-          ) : null}
-          <div className='card-body mx-auto'>
-            <h6 className='card-title '>
-              {this.state.name
-                .toLowerCase()
-                .split(' ')
-                .map(
-                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
-                )
-                .join(' ')}
-            </h6>
-          </div>
-        </Card>
+            {this.state.tooManyRequests ? (
+              <h6 className='mx-auto'>
+                <span className='badge badge-danger mt-2'>
+                  Too Many Requests
+                </span>
+              </h6>
+            ) : null}
+            <div className='card-body mx-auto'>
+              <h6 className='card-title '>
+                {this.state.name
+                  .toLowerCase()
+                  .split(' ')
+                  .map(
+                    letter =>
+                      letter.charAt(0).toUpperCase() + letter.substring(1)
+                  )
+                  .join(' ')}
+              </h6>
+            </div>
+          </Card>
+        </StyledLink>
       </div>
     );
   }
