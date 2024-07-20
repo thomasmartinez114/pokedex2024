943e058170f14885d835db00600af39ce544faad
Thomas Martinez <devtommie114@gmail.com>
2024-07-19 21:04:01 -0400

    installed and implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
 
 import backgroundImage from './pattern.png';
 
 function App() {
   return (
-    <div className='App' style={{ background: `url(${backgroundImage})` }}>
-      <NavBar />
-      <div className='container'>
-        <Dashboard />
+    <Router>
+      <div className='App' style={{ background: `url(${backgroundImage})` }}>
+        <NavBar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/pokemon/:pokemonIndex' element={<Pokemon />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
